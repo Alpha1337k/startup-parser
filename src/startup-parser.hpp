c8fcb253fb8fdc44ba05b29aac5cd761aea6d916
Alpha1337k <ozziekruithof@gmail.com>
2021-11-05 13:21:16 +0100

    Feat: now using vector as storage, arguments now in order of access

--- src/startup-parser.hpp
+++ src/startup-parser.hpp
@@ -1,51 +1,46 @@
 #ifndef STARTUP_PARSER
 #define STARTUP_PARSER
 
-#include <map>
+#include <vector>
 #include <string>
 
 class StartupParser
 {
 private:
-	std::map<std::string, std::string> _items;
-	std::pair<std::string, std::string> _last;
+	std::vector<std::pair<std::string, std::string> > _items;
 public:
 	StartupParser() {}
 	StartupParser(int argc, char **argv, bool skipfirst=true)
 	{
+		_items.reserve(argc);
 		std::string delim = "=";
 		for (int i = 0 + skipfirst; i < argc; i++)
-		{
-			
+		{			
 			std::string toadd(argv[i]);
-			_items[toadd.substr(0, toadd.find(delim))] = toadd.substr(toadd.find(delim) + delim.length(), std::string::npos);
-		}
-		if (argc >= 1)
-		{
-			std::string toadd(argv[argc - 1]);
-			_last = std::make_pair(toadd.substr(0, toadd.find(delim)), toadd.substr(toadd.find(delim) + delim.length(), std::string::npos));
+			_items.push_back(std::make_pair(toadd.substr(0, toadd.find(delim)), toadd.substr(toadd.find(delim) + delim.length(), std::string::npos)));
 		}
 	}
 	StartupParser(const StartupParser &sp) { *this = sp;}
 	StartupParser &operator=(const StartupParser &sp)
 	{
 		_items = sp._items;
-		_last = sp._last;
 
 		return *this;
 	}
 	std::string *operator[](std::string idx)
 	{
-		std::map<std::string, std::string>::iterator iter = _items.find(idx);
-		if (iter == _items.end())
-			return 0;
-		return &iter->second;
+		for (size_t i = 0; i < _items.size(); i++)
+		{
+			if (_items[i].first == idx)
+				return &_items[i].second;			
+		}
+		return 0;
 	}
 	std::pair<std::string, std::string>	&last()
 	{
-		return _last;
+		return _items.back();
 	}
-	std::map<std::string, std::string>	&getStorage()
+	std::vector<std::pair<std::string, std::string> >	&getStorage()
 	{
 		return _items;
 	}
--- test/main.cpp
+++ test/main.cpp
@@ -7,5 +7,11 @@ int main(int argc, char **argv)
 {
 	sp = StartupParser(argc, argv);
 	
+	std::vector<std::pair<std::string, std::string> > itm = sp.getStorage();
+	for (size_t i = 0; i < itm.size(); i++)
+	{
+		std::cout << itm[i].first << "===" << itm[i].second << std::endl;
+	}
+	
 	std::cout << sp["data"] << "/" << sp.last().second << std::endl;
 }
